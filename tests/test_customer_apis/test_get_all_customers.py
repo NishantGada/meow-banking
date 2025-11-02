eb518615e0d8f0db8feeec1c4691d20adf54188e
NishantGada <nishant.gada160@gmail.com>
2025-11-02 16:43:32 -0500

    refactor: updates tests for get all customers api

--- tests/test_customer_apis/test_get_all_customers.py
+++ tests/test_customer_apis/test_get_all_customers.py
@@ -6,4 +6,27 @@ def test_get_all_customers_success(client):
     assert "number_of_customers" in response.json()["data"]
     assert "all_customers" in response.json()["data"]
     assert type(response.json()["data"]["all_customers"]) == list
-    # assert response.json()["data"]["email"] == "test@example.com"
+
+
+def test_get_all_customers_with_entries(client):
+    client.post(
+        "/customers", json={"email": "test_1@example.com", "password": "testpass123"}
+    )
+    client.post(
+        "/customers", json={"email": "test_2@example.com", "password": "testpass123"}
+    )
+    client.post(
+        "/customers", json={"email": "test_3@example.com", "password": "testpass123"}
+    )
+
+    response = client.get("/customers")
+
+    assert response.status_code == 200
+    assert response.json()["data"]["number_of_customers"] == 3
+    assert (
+        len(response.json()["data"]["all_customers"])
+        == response.json()["data"]["number_of_customers"]
+    )
+    # assert response.json()["data"]["all_customers"][0] == isinstance(dict)
+    assert isinstance(response.json()["data"]["all_customers"][0], dict)
+    assert len(response.json()["data"]["all_customers"][0]) == 5
