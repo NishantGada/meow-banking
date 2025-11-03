aa8b04d47f05870bcd1329e4f32980e881d6975d
NishantGada <nishant.gada160@gmail.com>
2025-11-02 20:59:58 -0500

    refactor: updates types in schemas for complete accuracy and validation

--- apis/schemas.py
+++ apis/schemas.py
@@ -1,6 +1,8 @@
-import uuid
 from pydantic import BaseModel, Field, EmailStr
 from uuid import UUID
+
+# local imports
+from models.account import AccountTypeEnum
 
 
 class CustomerCreate(BaseModel):
@@ -14,21 +16,33 @@ class CustomerCreate(BaseModel):
 class CustomerUpdate(BaseModel):
     email: EmailStr | None = None
 
+    class Config:
+        extra = "forbid"
+
 
 class AccountCreate(BaseModel):
-    customer_id: uuid.UUID
+    customer_id: UUID
     initial_deposit: float = Field(gt=0)
     account_type: str = "checking"
 
+    class Config:
+        extra = "forbid"
+
 
 class AccountUpdate(BaseModel):
-    account_type: str | None = None
+    account_type: AccountTypeEnum | None = None
+
+    class Config:
+        extra = "forbid"
 
 
 class TransferCreate(BaseModel):
-    from_account_id: str
-    to_account_id: str
+    from_account_id: UUID
+    to_account_id: UUID
     amount: float = Field(gt=0)
+
+    class Config:
+        extra = "forbid"
 
 
 class PasswordUpdate(BaseModel):
@@ -40,14 +54,18 @@ class PasswordUpdate(BaseModel):
 
 
 class WithdrawSchema(BaseModel):
-    # user_id: UUID
-    user_id: str
-    account_id: str
+    user_id: UUID
+    account_id: UUID
     amount: float = Field(gt=0)
+
+    class Config:
+        extra = "forbid"
 
 
 class DepositSchema(BaseModel):
-    # user_id: UUID
-    user_id: str
-    account_id: str
+    user_id: UUID
+    account_id: UUID
     amount: float = Field(gt=0)
+
+    class Config:
+        extra = "forbid"
